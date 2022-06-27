1790e8f85c846957e6b4f3900a68c519fdc21b18
Marwh-S <marwa9777@hotmail.com>
2022-06-27 13:11:53 +0400

    design the general view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,21 +91,21 @@
                     Welcome to our E-Store
                 </div>
 
-                
+                <br>
+                <br>
+
                 <div class="subtitle ">
                     All You Need Is Shopping
                 </div>
 
+                <br>
+                <br>
+
                
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="about us.html" style="color:black;">About Us</a>
+                    <a href="our products.html" style="color:black;">Our Products</a>
+                    <a href="contact us.html" style="color:black;">Contact Us</a>
                 </div>
             </div>
         </div>
