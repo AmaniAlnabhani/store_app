3c78edf88a3c3542e9c898ca26c8aa9dbf0baa16
MariaMucize <108001542+MariaMucize@users.noreply.github.com>
2022-06-28 20:45:48 +0400

    Update dashboard.blade.php

--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -1,7 +1,23 @@
 @extends('customer.layouts.customer')
 @section('content')
-    
+
     <div class="alert alert-success" role="alert">
         Hello Customer
     </div>
+
+    <div class="container">
+        <div class="row">
+            @foreach ($users as $user)
+              <div class="col-12">
+
+                <ul>
+                    @foreach ($user->product as $product)
+                         <li>{{$product->all}}</li>
+                    @endforeach
+                </ul>
+
+            @endforeach
+        </div>
+    </div>
+
 @endsection
