daa1a189b518cb02847588f291d2e928ebc2356e
MariaMucize <108001542+MariaMucize@users.noreply.github.com>
2022-06-28 20:42:50 +0400

    Update ProductsController.php

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -14,21 +14,29 @@ public function __construct()
         $this->middleware('auth');
     }
 public function store(Request $request){
-    //return $request;
-    
-        $product = new Product();
+
+     if(Auth::check()){
+     $product = new Product();
      $product->name=$request->name;
      $product->price=$request->price;
      $product->section=$request->section;
      $product->quantity=$request->quantity;
      $product->attachment=$request->attachment;
-     
      $product->save();
-    
- return "Record is created succefuly";
+     return "Product is created Seccessfully";
 
+     }else{
+           return redirect('home');
+     }
 
-}
+    }
+
+    public function list()
+    {
+        $data['product'] = Product::all();
+
+        return view('product/list', $data);
+    }
 
 
 }
